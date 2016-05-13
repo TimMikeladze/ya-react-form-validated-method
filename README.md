3e558df74fe937066146d7b11e457f8ff2dcf3aa
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-05-13 03:29:55 -0700

    Initial commit with some work

---
+++ README.md
@@ -0,0 +1,4 @@
+# ya-react-form-validated-method
+Extends ya-react-form to streamline usage with mdg:validated-method
+
+[![Circle CI](https://circleci.com/gh/spacedustio/ya-react-form.svg?style=svg)](https://circleci.com/gh/spacedustio/ya-react-form-validated-method)
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { YaForm, FormActions } from 'meteor/ya-react-form';
+import { CallPromiseMixin as callPromiseMixin } from 'meteor/didericis:callpromise-mixin';
+import { ValidationError } from 'meteor/mdg:validation-error';
+
+YaForm.validatedMethod = ({ name, method }) => {
+  const validator = ({ form, dispatch }) => {
+    try {
+      method.validate(form);
+    } catch (exception) {
+      if (ValidationError.is(exception)) {
+        dispatch(FormActions.error({ formName: name, reason: exception.reason }));
+        exception.details.forEach(obj => {
+          dispatch(FormActions.invalidateField(
+            { formName: name, name: obj.name, reason: exception.type }
+          ));
+        });
+      } else {
+        throw new Error('ValidatedMethod\'s validate must throw an mdg:validation-error');
+      }
+    }
+  };
+  const makePromise = ({ form }) => {
+    const promise = !method.hasOwnProperty('callPromise') ? callPromiseMixin(method) : method;
+    return promise.callPromise({ form });
+  };
+  return YaForm.submit({ name, validator, method: makePromise });
+};
