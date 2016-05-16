7c9575ee40598c1996e89ac0d4e9e3bff10ebbb9
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-05-16 15:43:36 -0700

    saving work

--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
 import { YaForm, FormActions } from 'meteor/ya-react-form';
 import { CallPromiseMixin as callPromiseMixin } from 'meteor/didericis:callpromise-mixin';
 import { ValidationError } from 'meteor/mdg:validation-error';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 const isNotValidationErrorMsg = 'ValidatedMethod\'s validate must throw an mdg:validation-error';
 
-YaForm.validatedMethod = ({ name, method, onSubmit, onSuccess, onFailure, onValidation }) => {
-  const validator = ({ form, dispatch }) => {
-    try {
-      // Runs ValidatedMethod's validate function
-      method.validate(form);
-    } catch (exception) {
-      // Only accept valid error format
-      if (ValidationError.is(exception)) {
-        // Set the form level error
-        dispatch(FormActions.error({ formName: name, reason: exception.reason }));
-        // Set the field level errors
-        exception.details.forEach(obj => {
-          dispatch(FormActions.invalidateField(
-            { formName: name, name: obj.name, reason: obj.type }
-          ));
-        });
-      } else {
-        throw new Error(isNotValidationErrorMsg);
-      }
+YaForm.config.validator = ({ form, method, dispatch, schema }) => {
+  const options = schema && schema.hasOwnProperty('options') ? schema.options : {};
+  const simpleSchema = schema && schema.hasOwnProperty('simpleSchema') ? schema.simpleSchema : {};
+  try {
+    // Runs ValidatedMethod's validate function
+    debugger;
+    if (method && method.hasOwnProperty('validate')) {
+      method.validate(form, options);
+    } else if (simpleSchema instanceof SimpleSchema) {
+      const context = simpleSchema.newContext();
+      context.validate(form, options);
     }
-  };
+  } catch (exception) {
+      // Set the form level error
+    dispatch(FormActions.error({ formName: name, reason: exception.reason }));
+      // Set the field level errors
+    if (Array.isArray(exception.details)) {
+      exception.details.forEach(obj => {
+        dispatch(FormActions.invalidateField(
+              { formName: name, name: obj.name, reason: obj.type }
+            ));
+      });
+    }
+  }
+};
+
+YaForm.validatedMethod = (
+  { name, validator, method, onSubmit, onSuccess, onFailure, onValidation, schema }
+) => {
   // Add callPromise to ValidatedMethod if it doesn't exist.
   const makePromise = ({ form }) => {
     const promise = !method.hasOwnProperty('callPromise') ? callPromiseMixin(method) : method;
@@ -41,6 +50,7 @@ YaForm.validatedMethod = ({ name, method, onSubmit, onSuccess, onFailure, onVali
   // Let the magic happen
   return YaForm.submit(
     { name, validator, onSubmit, onSuccess,
-      onValidation, onFailure: onFailureWrapped, method: makePromise }
+      onValidation, onFailure: onFailureWrapped, schema,
+      method: makePromise }
   );
 };
