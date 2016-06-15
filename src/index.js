8ebd9c51160279a76e597b158d0ef44e0c87de18
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-06-15 12:43:30 -0700

    Saving work

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import { YaForm } from 'ya-react-form';
 
 class YaFormValidatedMethod extends YaForm {
   submit(name, validatedMethod) {
-    this.setValidator(({ name, form, schema, dispatch, state }) => {
+    this.setMethod(({ form }) =>
+      new Promise((resolve, reject) =>
+        validatedMethod.call(form, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        })
+      )
+    );
+
+    this.setOnFailure((err, args) => {
       debugger;
     });
     return super.submit(name);
