6f582ef60e5fcb7ca7b91766e9fc566426fcf3cb
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-06-29 00:32:15 -0700

    Extend Form component

--- src/Form.js
+++ src/Form.js
@@ -2,22 +2,18 @@ import React from 'react';
 import { Form } from 'ya-react-redux-form';
 import FormHandler from './FormHandler';
 
-class MeteorForm extends React.Component {
+class MeteorForm extends Form {
   constructor(props, context) {
     super(props, context);
     const { dispatch, getState } = this.store;
-    debugger;
     const { name, validatedMethod } = this.props;
     const args = { dispatch, getState, name, validatedMethod };
     this.handler = this.props.handler || new FormHandler(args);
   }
-  render() {
-    return <Form {...this.props} />;
-  }
 }
 
 MeteorForm.propTypes = {
-  validatedMethod: React.PropTypes.object.required,
+  validatedMethod: React.PropTypes.object.isRequired,
   name: React.PropTypes.string.isRequired,
   handler: React.PropTypes.object,
 };
